756eadfa06ab999a3b72e2a74d3eea63b10625e1
dagimalemux <133212131+dagimalemux@users.noreply.github.com>
2023-12-14 14:47:07 +0000

    url setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const url = "https://newsapi.org/v2/everything?q=tesla&from=2023-11-14&sortBy=publishedAt&apiKey=b7c134c9560e48a3ab450f3d2ab5b63b";
+
+async function dataRequest(){
+  try{const response = await fetch(url);
+  const data = response.json();
+
+  return data;
+     }catch(error){
+    console.log(error);
+     }
+}
+
+dataRequest(url).then(data => console.log(data))
